1cb28df57f941d73df78d02dd329e0eb54f16d56
rehh-joy87 <reemajoy281@gmail.com>
2026-07-28 15:55:16 +0530

    Add Express GET and POST routes

--- server.js
+++ server.js
@@ -1,13 +1,38 @@
 const express = require("express");
 
 const app = express();
-
 const PORT = 3000;
 
+// Middleware to read JSON data
+app.use(express.json());
+
+// GET Route
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Welcome to my Express.js Server!");
 });
 
+// GET Route for blogs
+app.get("/blogs", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            title: "My First Blog",
+            content: "This is my first blog post."
+        }
+    ]);
+});
+
+// POST Route
+app.post("/blogs", (req, res) => {
+    const blog = req.body;
+
+    res.status(201).json({
+        message: "Blog added successfully!",
+        data: blog
+    });
+});
+
+// Start server
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
