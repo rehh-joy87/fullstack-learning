d9e82bc3bdadedb87c9fb5632cc6fa09a30757b9
rehh-joy87 <reemajoy281@gmail.com>
2026-07-28 16:19:36 +0530

    Add blog API with JavaScript array storage

--- server.js
+++ server.js
@@ -3,32 +3,43 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-// Middleware to read JSON data
+// Middleware
 app.use(express.json());
 
-// GET Route
+// JavaScript array to store blogs
+const blogs = [
+    {
+        id: 1,
+        title: "My First Blog",
+        content: "This is my first blog post."
+    }
+];
+
+// Home Route
 app.get("/", (req, res) => {
     res.send("Welcome to my Express.js Server!");
 });
 
-// GET Route for blogs
+// GET all blogs
 app.get("/blogs", (req, res) => {
-    res.json([
-        {
-            id: 1,
-            title: "My First Blog",
-            content: "This is my first blog post."
-        }
-    ]);
+    res.json(blogs);
 });
 
-// POST Route
+// POST a new blog
 app.post("/blogs", (req, res) => {
-    const blog = req.body;
+    const { title, content } = req.body;
+
+    const newBlog = {
+        id: blogs.length + 1,
+        title,
+        content
+    };
+
+    blogs.push(newBlog);
 
     res.status(201).json({
         message: "Blog added successfully!",
-        data: blog
+        blog: newBlog
     });
 });
 
